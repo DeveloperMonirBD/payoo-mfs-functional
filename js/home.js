98777ad7548833343757cac02ac3f7a27d1bc79b
Monir <mrmonir0558@gmail.com>
2024-09-19 22:32:03 +0600

    Update the balance in the UI/DOM

--- js/home.js
+++ js/home.js
@@ -3,5 +3,26 @@ document.getElementById('add-money-btn').addEventListener('click', function (e) 
 
   const inputAddMoney = document.getElementById('input-add-money').value;
   const inputPinNumber = document.getElementById('input-pin-number').value;
-  console.log(inputAddMoney, inputPinNumber);
+  
+  // verify pin number :
+  // Wrong way to validate pin number:
+  if (inputPinNumber === '1234') {
+    console.log('Adding money to your account');
+  } else {
+    alert( 'failed to add money! please, try again later')
+  }
+
+  // get the current balance :
+  const balance = document.getElementById('balance').innerText;
+
+  // addMoneyInput with balance :
+  const addMoneyNumber = parseFloat(inputAddMoney);
+  const balanceNumber = parseFloat(balance);
+
+  const newBalance = balanceNumber + addMoneyNumber;
+  console.log(newBalance)
+
+  // update the balance in the UI/DOM
+  document.getElementById('balance').innerText = newBalance;
+
 });
