d3698db9d4cd9cd4a5ea567c8378c58936ce679d
Monir <mrmonir0558@gmail.com>
2024-09-19 21:51:37 +0600

    Addeded partially the add money even handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('Home addeded')
+document.getElementById('add-money-btn').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const inputAddMoney = document.getElementById('input-add-money').value;
+  const inputPinNumber = document.getElementById('input-pin-number').value;
+  console.log(inputAddMoney, inputPinNumber);
+});
